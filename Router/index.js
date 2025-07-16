64a3229205af176b7802635b99cab2286c7dc3c8
Faizullah <faizullahbalti29@gmail.com>
2025-07-16 15:00:45 +0500

    Routed for Update Url

--- Router/index.js
+++ Router/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const {handleAddUrl,handleGetAll, retriveOriginalUrl} = require('../Controller')
+const {handleAddUrl,handleGetAll, retriveOriginalUrl, updateUrl} = require('../Controller')
 const router = express.Router()
 
 router.route('/').post(handleAddUrl).get(handleGetAll)
-router.route('/:id').get(retriveOriginalUrl)
+router.route('/:id').get(retriveOriginalUrl).put(updateUrl)
 module.exports = router
